61865f65b6d12d80509e6f67c91fe08b72da6ec6
afc163 <afc163@gmail.com>
2015-10-21 16:54:52 +0800

    Updates

--- examples/common.js
+++ examples/common.js
@@ -143,6 +143,8 @@
 	  value: true
 	});
 	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
 	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -312,7 +314,7 @@
 	        if (typeof child.type === 'function') {
 	          return _react2['default'].createElement(
 	            'div',
-	            { ref: child.key, key: child.key },
+	            _extends({ ref: child.key, key: child.key }, child.props),
 	            _this4.state.childrenShow[child.key] ? child : null
 	          );
 	        }
@@ -386,7 +388,7 @@
 	        childrenShow: childrenShow
 	      });
 	      elements.forEach(function (elem) {
-	        elem.className = _this5.props.animatingClassName[0];
+	        elem.className = ' ' + _this5.props.animatingClassName[0];
 	      });
 	    }
 	  }, {
@@ -407,7 +409,7 @@
 	      var _this7 = this;
 	
 	      elements.forEach(function (elem) {
-	        elem.className = _this7.props.animatingClassName[1];
+	        elem.className += ' ' + _this7.props.animatingClassName[1];
 	      });
 	    }
 	  }, {
