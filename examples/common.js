ceb775d7311c6c3656652dbc7089c4214a0de70e
afc163 <afc163@gmail.com>
2015-10-23 14:10:38 +0800

    Updates

--- examples/common.js
+++ examples/common.js
@@ -143,8 +143,6 @@
 	  value: true
 	});
 	
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
 	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -314,11 +312,10 @@
 	        }
 	        // handle Component without props, like <App />
 	        if (typeof child.type === 'function') {
-	          return _react2['default'].createElement(
-	            'div',
-	            _extends({ ref: child.key, key: child.key }, child.props),
-	            _this4.state.childrenShow[child.key] ? child : null
-	          );
+	          return (0, _react.createElement)('div', {
+	            ref: child.key,
+	            key: child.key
+	          }, _this4.state.childrenShow[child.key] ? child : null);
 	        }
 	        return (0, _react.cloneElement)(child, {
 	          ref: child.key
