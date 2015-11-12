98ee08d801da47017d57dfa293818ca3335ee08f
afc163 <afc163@gmail.com>
2015-11-12 18:32:12 +0800

    update examples/empty-children.js

--- examples/empty-children.js
+++ examples/empty-children.js
@@ -2,7 +2,7 @@ import QueueAnim from 'rc-queue-anim';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const Item = React.createClass({
+const QueueItem = React.createClass({
   render() {
     return (
       <QueueAnim>
@@ -14,15 +14,32 @@ const Item = React.createClass({
   }
 });
 
+const Item = React.createClass({
+  render() {
+    return (
+      <div>Item</div>
+    );
+  }
+});
+
 const Page1 = React.createClass({
   render() {
     return (
-      <QueueAnim interval={3000}>
-        <Item key='1' />
-        <Item key='2' />
-        <Item key='3' />
-        <Item key='4' />
-      </QueueAnim>
+      <div>
+        <QueueAnim interval={1500}>
+          <Item key='1' />
+          <Item key='2' />
+          <Item key='3' />
+          <Item key='4' />
+        </QueueAnim>
+        <hr />
+        <QueueAnim interval={1500}>
+          <QueueItem key='1'>11</QueueItem>
+          <QueueItem key='2'>11</QueueItem>
+          <QueueItem key='3'>11</QueueItem>
+          <QueueItem key='4'>11</QueueItem>
+        </QueueAnim>
+      </div>
     );
   }
 });
