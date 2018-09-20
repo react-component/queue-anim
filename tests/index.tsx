1322ece2b55e0e2bbc378549d5bfed9ceaf5baa7
jljsj33 <liangjun.jiang@alipay.com>
2018-09-20 20:32:22 +0800

    add d.ts, close #51

---
+++ tests/index.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import QueueAnim from '../';
+
+export default () => (
+  <QueueAnim
+    type="left"
+    interval={100}
+    delay={[100, 200]}
+    duration={({ key, index }) => {
+      if (key === '1' || index === 2) {
+        return 200;
+      }
+      return 100;
+    }}
+    component="div"
+    forcedReplay={false}
+    animatingClassName={['enter', 'leave']}
+    appear={false}
+    ease="easeInOutCirc"
+    leaveReverse={false}
+    componentProps={{ id: 'Test' }}
+    onEnd={({ key, type }) => { }}
+  >
+    <div key="0">test</div>
+    <div key="1">test</div>
+    <div key="2">test</div>
+  </QueueAnim>
+);
