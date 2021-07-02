53f17442b2b76a539296da9fd677c4602474086f
jljsj <liangjun.jiang@alipay.com>
2021-07-02 21:46:43 +0800

    update index.d.ts

--- typings/global/index.d.ts
+++ typings/global/index.d.ts
@@ -1,3 +1,7 @@
 /// <reference path="import.d.ts" />
 declare module '*.css';
 declare module '*.less';
+declare module 'style-utils';
+declare module 'tween-functions';
+declare module 'raf';
+declare module 'flubber';
